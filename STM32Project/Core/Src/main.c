aa75084db0ed7db55f06ab7f9af469addd1532d1
DESKTOP-6PB2JMV\Phuc Toan <phuctoan0708@gmail.com>
2023-10-03 23:57:21 +0700

    update ex2

--- STM32Project/Core/Src/main.c
+++ STM32Project/Core/Src/main.c
@@ -213,20 +213,24 @@ int main(void)
 		  clearAll();
 		  switch(status){
 		  case 1:
-			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
 			  display7SEG(1);
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
+
 			  break;
 		  case 2:
-			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
 			  display7SEG(2);
+			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
+
 			  break;
 		  case 3:
-			  HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, RESET);
 			  display7SEG(3);
+			  HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, RESET);
+
 			  break;
 		  case 4:
-			  HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, RESET);
 			  display7SEG(0);
+			  HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, RESET);
+
 			  break;
 		  }
 		  status++;
